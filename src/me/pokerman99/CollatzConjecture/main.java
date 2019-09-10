5485912e6c309b892bbc98760ac1204d6480d237
s0511527 <troy.gidney@lethbridgecollege.ca>
2019-09-10 09:16:59 -0600

    Changed algorithm to handle 0's and changed data type from int to long

--- src/me/pokerman99/CollatzConjecture/main.java
+++ src/me/pokerman99/CollatzConjecture/main.java
@@ -1,39 +1,57 @@
 package me.pokerman99.CollatzConjecture;
 
+import org.jetbrains.annotations.Contract;
+
 import java.util.*;
 
 public class main {
 
-    static List<Integer> integerList = new ArrayList<>();
-    static Map<Integer, Integer> map = new HashMap();
+    static List<Long> integerList = new ArrayList<>();
+    static Map<Long, Long> map = new HashMap<>();
 
-    static int DISPLAYAMOUNT = 15;
-    static int RANDOMBOUND = 99;
+    static int DISPLAYAMOUNT = 30;
+    static int RANDOMBOUND = 999999000;
 
     public static void main(String[] args) {
         Random random = new Random();
-        for (int x = 0; x < DISPLAYAMOUNT; x++) integerList.add(random.nextInt(RANDOMBOUND));
+        for (int x = 0; x < DISPLAYAMOUNT; x++) integerList.add(Long.parseLong(String.valueOf(random.nextInt(RANDOMBOUND))));
 
         integerList.forEach(integer -> algorithm(integer));
-        System.out.println("OG Number\tTimes looped");
-        map.forEach((integer, integer2) -> System.out.println(integer + "\t\t\t" + integer2));
+        //algorithm(-1234564443);
+
+
+        System.out.println("All numbers are converted to positive with ABS\nOG Number\tTimes looped");
+        map.forEach((lon, lon2) -> System.out.println(lon + "\t\t" + lon2));
+        System.out.println("\n\nMost times looped\t" + Collections.max(map.values()));
 
     }
 
-    public static void algorithm(int integer) {
-        int counter = 0;
-        int control = integer;
+    public static void algorithm(long lon) {
+
+        long counter = 0;
+        long control = isNegative(lon) ? Math.abs(lon) : lon;
+
+        if (control==0) return;
         while (control != 1) {
             if (control % 2 == 0) { //Even
                 control = control / 2;
             } else {
-                control = 3 * control + 1;
+                control = 3 * control;
+                control++;
             }
             counter++;
         }
 
-        map.put(integer, counter);
+        map.put(isNegative(lon) ? Math.abs(lon) : lon, counter);
     }
 
+    @Contract(pure = true)
+    static boolean isNegative(long lon) {
+        if (lon > 0) {
+            return false;
+        } else {
+            return true;
+        }
+    }
 
 }
